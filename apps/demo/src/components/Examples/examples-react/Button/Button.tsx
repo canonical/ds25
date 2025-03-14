bdc688e1daaef1adece5d1360e2f399febb14068
Julie Muzina <jmuzina@jmuzina.io>
2025-03-14 17:13:27 -0400

    aaa

--- apps/demo/src/components/Examples/examples-react/Button/Button.tsx
+++ apps/demo/src/components/Examples/examples-react/Button/Button.tsx
@@ -1,24 +1,39 @@
-import {ReactElement, useEffect} from "react";
-import "./styles.css";
-import type {ExampleComponentProps} from "../types.js";
+import { ReactElement, useEffect, useState } from "react";
+import cssUrl from "./styles.css?url";
+import type { ExampleComponentProps } from "../types.js";
 import useConfig from "../useEvent.js";
-import root from 'react-shadow';
+import root from "react-shadow";
 
-const Button = ({
-                  className,
-                  id,
-                  styles,
-                }: ExampleComponentProps): ReactElement => {
+async function loadStylesheet(url) {
+  const response = await fetch(url);
+  const cssText = await response.text();
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(cssText);
+  return sheet;
+}
 
+const Button = ({ className, id, styles }: ExampleComponentProps): ReactElement => {
   const config = useConfig();
+  const [cssSheet, setCssSheet] = useState<CSSStyleSheet | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    loadStylesheet(cssUrl).then((sheet) => {
+      if (isMounted) {
+        setCssSheet(sheet);
+      }
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  console.log({ cssUrl });
 
   return (
-    <root.div style={{...styles, ...config.styles}}>
-      <div
-        id={id}
-        className={["ds", "example", className].filter(Boolean).join(" ")}
-      >
-        <button onClick={() => alert("I'm a button!")} type={"button"}>
+    <root.div style={{ ...styles, ...config.styles }} styleSheets={cssSheet ? [cssSheet] : []} mode={"closed"}>
+      <div id={id} className={["ds", "example", className].filter(Boolean).join(" ")}>
+        <button onClick={() => alert("I'm a button!")} type="button">
           Click me
         </button>
         {JSON.stringify(config)}
